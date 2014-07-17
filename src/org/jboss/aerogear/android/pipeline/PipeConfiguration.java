28c5c70de38785e9adc17bef04ddd7434892286c
Summers Pittman <secondsun@gmail.com>
2014-07-17 11:15:33 -0400

    Stubbing Pipeline with registrations and adding tests

---
+++ src/org/jboss/aerogear/android/pipeline/PipeConfiguration.java
@@ -0,0 +1,39 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright Red Hat, Inc., and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.aerogear.android.pipeline;
+
+import java.net.URL;
+import org.jboss.aerogear.android.pipeline.paging.PageConfig;
+
+/**
+ *
+ * @param <CONFIGURATION> configuration TODO: define this better
+ */
+public interface PipeConfiguration<CONFIGURATION extends PipeConfiguration<CONFIGURATION>> {
+
+    public <DATA> Pipe<DATA> forClass(Class<DATA> aClass);
+
+    public CONFIGURATION withUrl(URL url);
+
+    //TODO: create module classes maybe?
+    public CONFIGURATION module(Object module);
+
+    public CONFIGURATION pageConfig(PageConfig pageConfig);
+
+    public CONFIGURATION requestBuilder(RequestBuilder multipartBuilder);
+    
+}
---
+++ test/org/jboss/aerogear/android/impl/pipeline/ModularizedPipelineTest.java
@@ -0,0 +1,148 @@
+package org.jboss.aerogear.android.impl.pipeline;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import static junit.framework.Assert.assertEquals;
+import org.jboss.aerogear.android.Pipeline2;
+import org.jboss.aerogear.android.authentication.AuthenticationModule;
+import org.jboss.aerogear.android.authentication.impl.HttpBasicAuthenticationModule;
+import org.jboss.aerogear.android.authorization.AuthzModule;
+import org.jboss.aerogear.android.impl.helper.Data;
+import static org.jboss.aerogear.android.impl.helper.UnitTestUtils.getPrivateField;
+import static org.jboss.aerogear.android.impl.pipeline.PipeTypes.REST;
+import org.jboss.aerogear.android.pipeline.Pipe;
+import org.jboss.aerogear.android.pipeline.PipeConfiguration;
+import org.jboss.aerogear.android.pipeline.RequestBuilder;
+import org.jboss.aerogear.android.pipeline.paging.PageConfig;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.mockito.Mockito.mock;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class ModularizedPipelineTest {
+
+    private URL url;
+
+    @Before
+    public void setup() throws MalformedURLException {
+        url = new URL("http://server.com/context/");
+    }
+
+    @Test
+    public void defaultPipeSetup() {
+
+        Pipe newPipe = Pipeline2.config(PipeConfiguration.class, "data").withUrl(url).forClass(Data.class);
+
+        assertEquals("verifying the given URL", "http://server.com/context/data", newPipe.getUrl().toString());
+        assertEquals("verifying the type", REST, newPipe.getType());
+    }
+
+    /**
+     * This test will show that the Pipe URL Creation can be overriden
+     */
+    @Test
+    public void customPipeSetup() {
+        Pipe newPipe = Pipeline2.config(IStubPipeConfiguration.class, "custom").withUrl(url).id(5).forClass(Data.class);
+
+        assertEquals("verifying the given URL", "http://server.com/context/data/5", newPipe.getUrl().toString());
+    }
+
+    /**
+     * This test will show that auth can be added to a pipe.
+     *
+     * @throws java.lang.NoSuchFieldException thrown by Java, shouldn't happen if the test works.
+     * @throws java.lang.IllegalArgumentException thrown by Java, shouldn't happen if the test works.
+     * @throws java.lang.IllegalAccessException thrown by Java, shouldn't happen if the test works.
+     */
+    @Test
+    public void addAuthToPipe() throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+        AuthenticationModule basicModule = new HttpBasicAuthenticationModule(url);
+
+        Pipe newPipe = Pipeline2.config(PipeConfiguration.class, "auth").withUrl(url).module(basicModule).forClass(Data.class);
+
+        RestRunner runner = getPrivateField(newPipe, "restRunner", RestRunner.class);
+        AuthenticationModule module = getPrivateField(runner, "authModule", AuthenticationModule.class);
+
+        //For now we will assume that if the module is set correctly then 
+        //restRunner will call it correctly.
+        assertEquals(basicModule, module);
+
+        /*
+         TODO: Eventually we will want to break the dependency on the 
+         AuthenticationModule type from RestRunner.  This will happen 
+         during modularization and this test will be rewritten
+         */
+    }
+
+    /**
+     * This test will show that authz can be added to a pipe.
+     * 
+     * @throws java.lang.NoSuchFieldException thrown by Java, shouldn't happen if the test works.
+     * @throws java.lang.IllegalArgumentException thrown by Java, shouldn't happen if the test works.
+     * @throws java.lang.IllegalAccessException thrown by Java, shouldn't happen if the test works.
+     */
+    @Test
+    public void addAuthzToPipe() throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+        AuthzModule authzModule = mock(AuthzModule.class);
+
+        Pipe newPipe = Pipeline2.config(PipeConfiguration.class, "auth").withUrl(url).module(authzModule).forClass(Data.class);
+
+        RestRunner runner = getPrivateField(newPipe, "restRunner", RestRunner.class);
+        AuthzModule module = getPrivateField(runner, "authzModule", AuthzModule.class);
+
+        //For now we will assume that if the module is set correctly then 
+        //restRunner will call it correctly.
+        assertEquals(authzModule, module);
+
+        /*
+         TODO: Eventually we will want to break the dependency on the 
+         AuthzModule type from RestRunner.  This will happen 
+         during modularization and this test will be rewritten
+         */    }
+
+    /**
+     * This test will show paging handling can be added to a Pipeline
+     *
+     */
+    @Test
+    public void addPagingToPipe() {
+        PageConfig pageConfig = new PageConfig();
+        Pipe newPipe = Pipeline2.config(PipeConfiguration.class, "auth").withUrl(url).pageConfig(pageConfig).forClass(Data.class);
+        
+        throw new IllegalStateException("Not yet implemented");
+        /*
+          TODO: Refactor paging perhaps to be more modular?
+          Refactor PageConfig to extend PipeConfiguration and make the builder more fluent?
+        */
+    }
+
+    /**
+     * This test will show that the upload mechanism can be swapped to
+     * multipart.
+     * 
+     * @throws java.lang.NoSuchFieldException thrown by Java, shouldn't happen if the test works.
+     * @throws java.lang.IllegalArgumentException thrown by Java, shouldn't happen if the test works.
+     * @throws java.lang.IllegalAccessException thrown by Java, shouldn't happen if the test works.
+     */
+    @Test
+    public void addMultipartToPipe() throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+        RequestBuilder multipartBuilder = new MultipartRequestBuilder();
+        Pipe newPipe = Pipeline2.config(PipeConfiguration.class, "auth").withUrl(url).requestBuilder(multipartBuilder).forClass(Data.class);
+        
+        RestRunner runner = getPrivateField(newPipe, "restRunner", RestRunner.class);
+        RequestBuilder runnerBuilder = runner.getRequestBuilder();
+        assertEquals(multipartBuilder, runnerBuilder);
+        
+        /*
+        TODO: What should happen when requestBuilders are chained?
+         */
+    }
+
+    private static interface IStubPipeConfiguration extends PipeConfiguration<IStubPipeConfiguration> {
+
+        IStubPipeConfiguration id(int id);
+    }
+
+}
