0e0768c2700e78c3cca8e93f79cff569e1aa39d1
Summers Pittman <secondsun@gmail.com>
2014-07-19 13:32:33 -0400

    Creating core modules.
    These will eventually be implemented by authModule and authzModule

---
+++ src/org/jboss/aerogear/android/code/PipeModule.java
@@ -0,0 +1,13 @@
+package org.jboss.aerogear.android.code;
+
+/**
+ * A PipeModule allows special actions to be taken during certain phases of
+ * the Pipe life cycle.
+ * 
+ * @author summers
+ */
+public interface PipeModule {
+    
+    
+    
+}
