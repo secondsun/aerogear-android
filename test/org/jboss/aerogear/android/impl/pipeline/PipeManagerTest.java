241551898d35cd5ba3ad14a1d71b0e527d2fb8d2
Summers Pittman <secondsun@gmail.com>
2014-08-06 12:20:21 -0400

    Checkstyle?  More like WRECKSTYLE"

--- test/org/jboss/aerogear/android/impl/pipeline/PipeManagerTest.java
+++ test/org/jboss/aerogear/android/impl/pipeline/PipeManagerTest.java
@@ -20,7 +20,6 @@ import org.jboss.aerogear.android.pipeline.ResponseParser;
 import org.jboss.aerogear.android.pipeline.paging.PageConfig;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import static org.mockito.Mockito.mock;
